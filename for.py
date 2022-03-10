30392618f56ca18fdef8df3520407703d876208a
simha amara <simhaamara2020@gmail.com>
2022-03-10 10:25:25 +0200

    ex for python

---
+++ for.py
@@ -0,0 +1,33 @@
+
+#1 for
+num =0
+for i in range(1,20):
+    if i %2!=0:
+       print(i)
+
+
+
+#3
+num=1
+N = int(input("please enter number :"))
+for num in range(1,N):
+    if num % 2 != 0:
+        print(num)
+        num += 1
+
+
+
+#4
+num1 = int(input("enter number 1/2:"))
+num1  = 1
+for num1 in range(1,20):
+    if num1 % 2 != 0:
+        print(num1)
+
+
+#8
+x=int(input("enter number"))
+y=int(input("enter number"))
+for x in range(1,5,1):
+    if x != 1:
+        print(x)
