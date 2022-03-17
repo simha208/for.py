b59a2b749ddcd4641dc7bc4475c99728656baf0c
simha amara <simhaamara2020@gmail.com>
2022-03-17 10:56:02 +0200

    ex for python

---
+++ for.2.py
@@ -0,0 +1,22 @@
+#1
+# print("I Love python "*5)
+# #2
+# n=[0,1,2,3,4,5,6,7,8,9,10]
+# print(n)
+#3
+
+#4
+l=list()
+sum=0
+for i in range(11):
+   num=input("please enter number:")
+   l.append(int(num))
+   sum+= int(num)
+   print(sum)
+
+#4.A
+# l=[10,20,30,40,50,60,12,14,18,19,21]
+# for j in l:
+#     print(j/11)
+
+#4.B
--- for.py
+++ for.py
@@ -1,33 +1,64 @@
 
 #1 for
-num =0
-for i in range(1,20):
-    if i %2!=0:
-       print(i)
+# num =0
+# for i in range(1,20):
+#     if i %2!=0:
+#        print(i)
 
 
 
 #3
-num=1
-N = int(input("please enter number :"))
-for num in range(1,N):
-    if num % 2 != 0:
-        print(num)
-        num += 1
+# num=1
+# N = int(input("please enter number :"))
+# for num in range(1,N):
+#     if num % 2 != 0:
+#         print(num)
+#         num += 1
 
 
 
 #4
-num1 = int(input("enter number 1/2:"))
-num1  = 1
-for num1 in range(1,20):
-    if num1 % 2 != 0:
-        print(num1)
+# num = int(input("please enter number :"))
+# x= int(input("please enter number1 to odd number \nor 2 to even number :"))
+# if x== 1:
+#       for i in range(1,num,2):
+#          print(i)
+# elif x ==2:
+#      for i in range(0,num,2):
+#         print(i)
+# else:
+#        print("Insert number 1 or 2")
+#
+# #6
+# x=1
+# y=14
+# sum=0
+#
+# for i in range(sum,y-1):
+#     for j in range(sum,x+1):
+#         print(i + j, end=',')
+#
+#
+
+# 7
+# x=10
+# y=10
+# for i in range(1,y+1):
+#     for j in range(1,x+1):
+#           print(i * j,end= '|')
+#     print()
 
 
 #8
-x=int(input("enter number"))
-y=int(input("enter number"))
-for x in range(1,5,1):
-    if x != 1:
-        print(x)
+# num=input("enter 5 number")
+# num2=input("enter 5 number")
+# for i in range(len(num)):
+#     if num[i] == num2[i]:
+#         print(num[i])
+
+#9
+# num=input("enter 5 number")
+# num2=input("enter 1 number")
+# for i in range(len(num)):
+#        if num2 == num[i]:
+#           print(num2)
